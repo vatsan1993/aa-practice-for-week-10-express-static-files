393440dfa25f795bd60137ae9cf2eb9722eba79b
Srivatsan <vatsan1993@gmail.com>
2024-09-21 20:34:06 +0530

    phase 1 finished

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // Your code here
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'assets/')));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
