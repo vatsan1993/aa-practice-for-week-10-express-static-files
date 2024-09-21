0b0243fbf6dc37b7c42fa78c641b9bc002bd6df4
Srivatsan <vatsan1993@gmail.com>
2024-09-21 21:34:56 +0530

    finished task 3

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 // app.use(express.static(path.join(__dirname, 'assets')));
 
-app.use(express.static(path.join(__dirname, 'assets/scripts')));
+// app.use(express.static(path.join(__dirname, 'assets/scripts')));
+
+app.use('/stylesheets', express.static(path.join(__dirname, 'assets/css')));
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
